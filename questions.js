cb23884f90647f67637902a74b1695ef71731cc7
awsteam7 <61358994+awsteam7@users.noreply.github.com>
2020-02-22 15:53:51 -0800

    Initial commit

---
+++ questions.js
@@ -0,0 +1,36 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+ 
+  QUESTIONS_EN_US: [
+    {
+      'Do you have a fever?  ': [
+        'Yes',
+        'No',
+      ],
+    },
+    {
+      'Do you have a cough?  ': [
+        'Yes',
+        'No',
+      ],
+    },
+    {
+      'Do you have a runny nose?  ': [
+       'Yes',
+        'No',
+      ],
+    },
+    {
+      'Have you been sick for longer than 3 days?  ': [
+       'Less then 3 days',
+        'More than 3 days',
+      ],
+    },
+  ],
+ 
+};
